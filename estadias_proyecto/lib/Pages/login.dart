83ba0e54b473c802433bc427fe99f0b33a9df911
Kevin12-0 <1719110155@utectulancingo.edu.mx>
2021-09-20 21:05:22 -0500

    login_theme

--- estadias_proyecto/lib/Pages/login.dart
+++ estadias_proyecto/lib/Pages/login.dart
@@ -10,13 +10,38 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.blue[100],
+      backgroundColor:Colors.grey,
       body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 90.0,
+        ),
         children: <Widget>[
-          CircleAvatar(
-            radius: 100.0,
-            backgroundColor:Colors.grey[100],
-            backgroundImage: AssetImage('images/img.png'),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100.0,
+                backgroundColor:Colors.grey,
+                backgroundImage: AssetImage('images/img.png'),
+              ),
+              Text(
+                'INICIAR SESION',
+                style: TextStyle(
+                  fontFamily: 'Linebeam',
+                  fontSize: 25.0
+                ),
+              ),
+              SizedBox(
+                width:160.0,
+                height:15.0,
+                child: 
+                Divider(
+                  color: Colors.grey[900],
+                )
+              ),
+              
+            ],
           )
         ],
       ),
